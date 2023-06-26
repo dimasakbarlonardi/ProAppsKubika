be0e2935f7669e624ecca68f53b0258b80f20156
akmal <satoefrekuensi@gmail.com>
2023-06-27 06:33:25 +0700

    transaction center

--- app/Http/Controllers/Admin/WorkOrderController.php
+++ app/Http/Controllers/Admin/WorkOrderController.php
@@ -466,8 +466,6 @@ class WorkOrderController extends Controller
             $createTransaction->status = 'PENDING';
             $createTransaction->save();
 
-            $midtrans = new CreateSnapTokenService($createTransaction, $items, $admin_fee);
-
             $ct = $this->transactionCenter($createTransaction);
 
             $midtrans = new CreateSnapTokenService($ct, $createTransaction, $items, $admin_fee);
