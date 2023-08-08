c72f2f069b5a5beb9301c8069a5518c97cf6e4b2
Dimas Akbar Lonardi <dimasakbarlonardi@yahoo.com>
2023-08-08 13:10:35 +0700

    updatepullakmal

--- resources/views/AdminSite/ChecklistAhuH/create.blade.php
+++ resources/views/AdminSite/ChecklistAhuH/create.blade.php
@@ -14,22 +14,18 @@
                 @csrf
                 <div class="row">
                 <div class="col-6 mb-3">
-                    <label class="form-label">Barcode Room</label>
-                    <input type="text" name="barcode_room" class="form-control" required>
-                </div>
-                {{-- <div class=" col-6 mb-3">
-                    <label class="form-label">Nomer Checklist AHU</label>
-                    <input type="text" name="no_checklist_ahu" class="form-control" required>
-                </div> --}}
-                {{-- <div class=" col-6 mb-3">
-                    <label class="form-label">Tanggal Checklist</label>
-                    <input type="date" name="tgl_checklist" class="form-control" required>
+                    <label class="form-label">No. Equiqment</label>
+                    <input type="text" name="no_equiqment" class="form-control" required>
                 </div>
                 <div class=" col-6 mb-3">
-                    <label class="form-label">Time Checklist</label>
-                    <input type="time" name="time_checklist" class="form-control" required>
-                </div> --}}
-                <div class="col-6 mb-3">
+                    <label class="form-label">Nama Equiqment</label>
+                    <input type="text" name="nama_equiqment" class="form-control" required>
+                </div>
+                <div class=" col-6 mb-3">
+                    <label class="form-label">PIC</label>
+                    <input type="date" name="tgl_checklist" class="form-control" required>
+                </div>
+                {{-- <div class="col-6 mb-3">
                     <label class="form-label">Eng AHU</label>
                     <select class="form-control" name="id_barcode_ahu" required>
                         <option selected disabled>-- Pilih Enggeneering AHU --</option>
@@ -37,33 +33,16 @@
                         <option value="{{ $engahu->id_eng_ahu }}">{{ $engahu->nama_eng_ahu }} </option>
                         @endforeach
                     </select>
-                </div>
+                </div> --}}
 
                 <div class="col-6 mb-3">
-                    <label class="form-label">Room</label>
+                    <label class="form-label">Lokasi</label>
                     <select class="form-control" name="id_room" required>
-                        <option selected disabled>-- Pilih Room --</option>
+                        <option selected disabled>-- Pilih Lokasi --</option>
                         @foreach ($rooms as $room)
                         <option value="{{ $room->id_room }}">{{ $room->nama_room }} </option>
                         @endforeach
                     </select>
-                </div>
-                
-                <div class="row mt-4">
-                    <h6>ISI DETAIL CHECKLIST AHU <hr></h6>
-
-                    <div class=" col-6 mb-3">
-                        <label class="form-label">IN / OUT</label>
-                        <input type="text" name="in_out" class="form-control" required>
-                    </div>
-                    <div class=" col-6 mb-3">
-                        <label class="form-label">Check Point</label>
-                        <input type="text" name="check_point" class="form-control" required>
-                    </div>
-                    <div class=" col-6 mb-3">
-                        <label class="form-label">Keterangan</label>
-                        <input type="text" name="keterangan" class="form-control" required>
-                    </div>
                 </div>
                 <div class="mt-5">
                     <button type="submit" class="btn btn-primary">Submit</button>
