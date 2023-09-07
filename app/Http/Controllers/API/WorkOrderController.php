96604829c839b1354b9473105fbe3746a39cbfc5
akmal <satoefrekuensi@gmail.com>
2023-09-07 17:05:00 +0700

    API | Approve WO by tenant

--- app/Http/Controllers/API/WorkOrderController.php
+++ app/Http/Controllers/API/WorkOrderController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers\API;
 
+use Carbon\Carbon;
+use App\Models\System;
+use App\Models\WorkOrder;
+use App\Models\CashReceipt;
+use Illuminate\Http\Request;
 use App\Helpers\ConnectionDB;
+use App\Helpers\HelpNotifikasi;
+use Illuminate\Support\Facades\DB;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
-use App\Models\WorkOrder;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
+
 
 class WorkOrderController extends Controller
 {
@@ -36,9 +41,72 @@ class WorkOrderController extends Controller
         $wo->date_approve_1 = Carbon::now();
         $wo->save();
 
+        $createTransaction = $this->createTransaction($wo);
+
+        HelpNotifikasi::paymentWO($id, $createTransaction);
+
         return ResponseFormatter::success(
             $wo,
             'Success approve WO'
         );
+    }
+
+    public function createTransaction($wo)
+    {
+        $connSystem = ConnectionDB::setConnection(new System());
+        $connTransaction = ConnectionDB::setConnection(new CashReceipt());
+
+        $system = $connSystem->find(1);
+        $request = Request();
+        $user = $request->user();
+
+        $count = $system->sequence_no_cash_receiptment + 1;
+        $countINV = $system->sequence_no_invoice + 1;
+
+        $no_cr = $system->kode_unik_perusahaan . '/' .
+            $system->kode_unik_cash_receipt . '/' .
+            Carbon::now()->format('m') . Carbon::now()->format('Y') . '/' .
+            sprintf("%06d", $count);
+
+        $no_inv = $system->kode_unik_perusahaan . '/' .
+            $system->kode_unik_invoice . '/' .
+            Carbon::now()->format('m') . Carbon::now()->format('Y') . '/' .
+            sprintf("%06d", $countINV);
+
+        $order_id = $user->id_site . '-' . $no_cr;
+
+        try {
+            DB::beginTransaction();
+
+            $createTransaction = $connTransaction;
+            $createTransaction->order_id = $order_id;
+            $createTransaction->id_site = $user->id_site;
+            $createTransaction->no_reff = $wo->no_work_order;
+            $createTransaction->no_draft_cr = $no_cr;
+            $createTransaction->ket_pembayaran = 'WO/' . $wo->Ticket->Tenant->User->id_user . '/' . $wo->Ticket->Unit->nama_unit;
+            $createTransaction->sub_total = $wo->jumlah_bayar_wo;
+            $createTransaction->no_invoice = $no_inv;
+            $createTransaction->transaction_status = 'PENDING';
+            $createTransaction->id_user = $wo->Ticket->Tenant->User->id_user;
+            $createTransaction->transaction_type = 'WorkOrder';
+            $createTransaction->save();
+
+            $system->sequence_no_cash_receiptment = $count;
+            $system->sequence_no_invoice = $countINV;
+
+            $createTransaction->WorkOrder->Ticket->no_invoice = $no_inv;
+            $createTransaction->WorkOrder->Ticket->save();
+
+            $system->save();
+
+            DB::commit();
+        } catch (Throwable $e) {
+            dd($e);
+            DB::rollBack();
+
+            return redirect()->back();
+        }
+
+        return $createTransaction;
     }
 }
