28a5afc90544127288c750ec098664a95598019b
akmal <satoefrekuensi@gmail.com>
2023-09-18 21:23:58 +0700

    API | Add image to package store

--- app/Http/Controllers/API/PackageController.php
+++ app/Http/Controllers/API/PackageController.php
@@ -11,6 +11,8 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Laravel\Sanctum\PersonalAccessToken;
+use File;
+use Illuminate\Support\Facades\Storage;
 
 class PackageController extends Controller
 {
@@ -19,8 +21,9 @@ class PackageController extends Controller
         $connPackage = ConnectionDB::setConnection(new Package());
         $time = Carbon::now()->format('dmY');
         $package_receipt_number = rand(0, 1000);
+        $package_receipt_number = $time . $package_receipt_number;
 
-        $connPackage->package_receipt_number = $time . $package_receipt_number;
+        $connPackage->package_receipt_number = $package_receipt_number;
         $connPackage->received_location = $request->received_location;
         $connPackage->receive_date = $request->receive_date;
         $connPackage->receive_time = $request->receive_time;
@@ -31,6 +34,17 @@ class PackageController extends Controller
         $connPackage->id_site = $request->id_site;
         $connPackage->GenerateBarcode();
         $connPackage->status = 'Arrived';
+
+        $file = $request->file('image');
+        if ($file) {
+            $fileName = $connPackage->package_receipt_number . '-' .   $file->getClientOriginalName();
+            $outputPackageImage = '/public/' . $request->user()->id_site . '/img/package/' . $fileName;
+            $packageImage = '/storage/' . $request->user()->id_site . '/img/package/' . $fileName;
+
+            Storage::disk('local')->put($outputPackageImage, File::get($file));
+
+            $connPackage->image = $packageImage;
+        }
         $connPackage->save();
 
         return ResponseFormatter::success(
