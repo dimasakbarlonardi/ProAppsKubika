bc63915ecb76a91f0de7a6e1a93de95360dec84c
akmal <satoefrekuensi@gmail.com>
2023-09-21 20:57:04 +0700

    MERGE | Merge from dimas

--- resources/views/AdminSite/ChecklistAhuDetail/index.blade.php
+++ resources/views/AdminSite/ChecklistAhuDetail/index.blade.php
@@ -86,11 +86,6 @@
                                 tabindex="-1" role="dialog" aria-hidden="true">
                                 <div class="modal-dialog modal-dialog-centered" role="document" style="max-width: 500px">
                                     <div class="modal-content position-relative">
-                                        <div class="position-absolute top-0 end-0 mt-2 me-2 z-index-1">
-                                            <button
-                                                class="btn-close btn btn-sm btn-circle d-flex flex-center transition-base"
-                                                data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
                                         <img id="modal-image"
                                             src="{{ $detail->image ? asset($detail->image) : asset('/assets/img/team/3-thumb.png') }}"
                                             alt="{{ $detail->image }}" class="img-thumbnail">
