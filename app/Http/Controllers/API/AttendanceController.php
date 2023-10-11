6772d80b2f92f35da0fae2a879d88742b2f53550
Akmal Rifqi <satoefrekuensi@gmail.com>
2023-10-11 13:26:05 +0700

    Server | Fix attendance checkin, checkout, & radius

--- app/Http/Controllers/API/AttendanceController.php
+++ app/Http/Controllers/API/AttendanceController.php
@@ -65,25 +65,21 @@ class AttendanceController extends Controller
 
         if ($tokenable) {
             $user = $tokenable->tokenable;
-            $site = Site::find($user->id_site);
-
+         
             $karyawan = $connKaryawan->where('email_karyawan', $user->email)->first();
             $attend = $this->attend($karyawan);
-            // dd($attend);
+           
             if ($karyawan->NowSchedule) {
                 if ($attend) {
-                    $site_lat = $site->lat;
-                    $site_long = $site->long;
                     $my_lat = $request->my_lat;
                     $my_long = $request->my_long;
 
-                    $distance = $this->getDistance($site_lat, $site_long, $my_lat, $my_long);
+                    $distance = $this->getDistance($my_lat, $my_long);
 
                     $checkin = Carbon::now()->format('H:i');
-
-                    if ($distance < 0.03) {
+                    
+                    if ($distance < 10) {
                         $start_hour = $karyawan->NowSchedule->ShiftType->checkin;
-                        $start_hour = '22:00'; //temporary just for dev
                         if ($checkin > $start_hour) {
                             $status_absence = 'Late';
                         } elseif ($checkin < $start_hour) {
@@ -91,18 +87,10 @@ class AttendanceController extends Controller
                         } elseif ($checkin == $start_hour) {
                             $status_absence = 'On Time';
                         }
-                        // dd($karyawan->NowSchedule);
-                        // $connAttend = ConnectionDB::setConnection(new Attendance());
+
                         $karyawan->NowSchedule->check_in = Carbon::now();
                         $karyawan->NowSchedule->status_absence = $status_absence;
                         $karyawan->NowSchedule->save();
-                        // $connAttend->create([
-                        //     'id_site' => $site->id_site,
-                        //     'id_user' => $attend['getUser']->id_user,
-                        //     'check_in' =>  Carbon::now(),
-                        //     'status' => 'On Work',
-                        //     'status_absence' => $status_absence
-                        // ]);
 
                         return response()->json([
                             'status' => 'OK',
@@ -135,10 +123,7 @@ class AttendanceController extends Controller
         $lat = $request->lat;
         $long = $request->long;
 
-        $site_lat = $site->lat;
-        $site_long = $site->long;
-
-        $distance = $this->getDistance($site_lat, $site_long, $lat, $long);
+        $distance = $this->getDistance($lat, $long);
         if ($distance < 0.03) {
             $resp = "Within 30 meter radius";
         } else {
@@ -162,15 +147,14 @@ class AttendanceController extends Controller
         if ($tokenable) {
             if ($attend && !$attend->checkout) {
                 if ($attend) {
-                    $site_lat = $site->lat;
-                    $site_long = $site->long;
+
                     $my_lat = $request->my_lat;
                     $my_long = $request->my_long;
 
-                    $distance = $this->getDistance($site_lat, $site_long, $my_lat, $my_long);
+                    $distance = $this->getDistance($my_lat, $my_long);
                     $checkin = new DateTime($attend->check_in);
                     $checkout = Carbon::now();
-                    $checkout = '2023-01-02 09:00'; //temporary just for dev
+                    
                     $work_hour = $checkin->diff(new DateTime($checkout));
 
 
@@ -180,7 +164,7 @@ class AttendanceController extends Controller
                         $work_hour = $work_hour->format('%h') . " Hours " . $work_hour->format('%i') . " Minutes";
                     }
 
-                    if ($distance < 0.03) {
+                    if ($distance < 10) {
                         $karyawan->NowSchedule->work_hour = $work_hour;
                         $karyawan->NowSchedule->check_out = $checkout;
                         $karyawan->NowSchedule->save();
@@ -215,18 +199,33 @@ class AttendanceController extends Controller
     }
 
 
-    function getDistance($latitude1, $longitude1, $latitude2, $longitude2)
+    function getDistance($latitude2, $longitude2)
     {
         $earth_radius = 6371;
 
-        $dLat = deg2rad($latitude2 - $latitude1);
-        $dLon = deg2rad($longitude2 - $longitude1);
+        $conCoordinates = ConnectionDB::setConnection(new Coordinate());
 
-        $a = sin($dLat / 2) * sin($dLat / 2) + cos(deg2rad($latitude1)) * cos(deg2rad($latitude2)) * sin($dLon / 2) * sin($dLon / 2);
-        $c = 2 * asin(sqrt($a));
-        $d = $earth_radius * $c;
+        $coordinates = $conCoordinates->get();
 
-        return $d;
+        foreach ($coordinates as $item) {
+            $latitude1 = $item->lat;
+            $longitude1 = $item->long;
+        }
+
+        if ($latitude1 && $longitude1) {
+            $dLat = deg2rad($latitude2 - $latitude1);
+            $dLon = deg2rad($longitude2 - $longitude1);
+
+            $a = sin($dLat / 2) * sin($dLat / 2) + cos(deg2rad($latitude1)) * cos(deg2rad($latitude2)) * sin($dLon / 2) * sin($dLon / 2);
+            $c = 2 * asin(sqrt($a));
+            $d = $earth_radius * $c;
+
+            return $d;
+        } 
+       
+        $resp = "Outside 10000 meter radius";
+
+        return response()->json(['status' => $resp]);        
     }
 
     public function shiftSchedule($userID)
