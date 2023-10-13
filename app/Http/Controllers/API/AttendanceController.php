cec6fc971ca8e9c0ce1c3f2a3b796253e4a1d0ef
akmal <satoefrekuensi@gmail.com>
2023-10-13 13:41:40 +0700

    merge staging

--- app/Http/Controllers/API/AttendanceController.php
+++ app/Http/Controllers/API/AttendanceController.php
@@ -240,30 +240,11 @@ class AttendanceController extends Controller
         if ($latitude1 && $longitude1) {
             $dLat = deg2rad($latitude2 - $latitude1);
             $dLon = deg2rad($longitude2 - $longitude1);
-        $conCoordinates = ConnectionDB::setConnection(new Coordinate());
-
-        $coordinates = $conCoordinates->get();
-
-        foreach ($coordinates as $item) {
-            $latitude1 = $item->lat;
-            $longitude1 = $item->long;
-        }
-
-        if ($latitude1 && $longitude1) {
-            $dLat = deg2rad($latitude2 - $latitude1);
-            $dLon = deg2rad($longitude2 - $longitude1);
 
             $a = sin($dLat / 2) * sin($dLat / 2) + cos(deg2rad($latitude1)) * cos(deg2rad($latitude2)) * sin($dLon / 2) * sin($dLon / 2);
             $c = 2 * asin(sqrt($a));
             $d = $earth_radius * $c;
 
-            return $d;
-        }
-
-        $resp = "Outside 10000 meter radius";
-
-        return response()->json(['status' => $resp]);
-    }
             return $d;
         }
 
