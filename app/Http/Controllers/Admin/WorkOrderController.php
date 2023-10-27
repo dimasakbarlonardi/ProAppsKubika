4ab680bbf7b51a1ad50afdc2442a973da2174ea1
akmal <satoefrekuensi@gmail.com>
2023-10-27 11:46:00 +0700

    WEB | Generate invoice number after finance approve at WO

--- app/Http/Controllers/Admin/WorkOrderController.php
+++ app/Http/Controllers/Admin/WorkOrderController.php
@@ -39,9 +39,7 @@ class WorkOrderController extends Controller
     }
 
     public function filteredData(Request $request)
-    {
-
-    }
+    { }
 
     public function store(Request $request)
     {
@@ -525,8 +523,12 @@ class WorkOrderController extends Controller
             sprintf("%06d", $count);
 
         $order_id = $user->id_site . '-' . $no_cr;
+        $countINV = $system->sequence_no_invoice + 1;
 
-        $admin_fee = 5000;
+        $no_inv = $system->kode_unik_perusahaan . '/' .
+            $system->kode_unik_invoice . '/' .
+            Carbon::now()->format('m') . Carbon::now()->format('Y') . '/' .
+            sprintf("%06d", $countINV);
 
         try {
             DB::beginTransaction();
@@ -537,14 +539,17 @@ class WorkOrderController extends Controller
             $createTransaction->no_reff = $wo->no_work_order;
             $createTransaction->no_draft_cr = $no_cr;
             $createTransaction->ket_pembayaran = 'WO/' . $wo->Ticket->Tenant->User->id_user . '/' . $wo->Ticket->Unit->nama_unit;
-            $createTransaction->admin_fee = $admin_fee;
             $createTransaction->sub_total = $wo->jumlah_bayar_wo;
-            $createTransaction->gross_amount = $wo->jumlah_bayar_wo + $admin_fee;
+            $createTransaction->no_invoice = $no_inv;
             $createTransaction->transaction_status = 'PENDING';
             $createTransaction->id_user = $wo->Ticket->Tenant->User->id_user;
             $createTransaction->transaction_type = 'WorkOrder';
             $createTransaction->save();
 
+            $wo->Ticket->no_invoice = $no_inv;
+            $wo->Ticket->save();
+
+            $system->sequence_no_invoice = $countINV;
             $system->sequence_no_cash_receiptment = $count;
             $system->save();
 
