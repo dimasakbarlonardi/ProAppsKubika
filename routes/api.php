331559b1253c0dcf02daf9f72a7c57a7cf0caa1b
akmal <satoefrekuensi@gmail.com>
2023-10-28 23:29:59 +0700

    API | Accept wo from get to post

--- routes/api.php
+++ routes/api.php
@@ -74,7 +74,7 @@ Route::prefix('v1')->group(function () {
 
         // Work Order
         Route::get('/work-order/{id}', [WorkOrderController::class, 'show']);
-        Route::get('/accept/work-order/{id}', [WorkOrderController::class, 'acceptWO']);
+        Route::post('/accept/work-order/{id}', [WorkOrderController::class, 'acceptWO']);
         Route::post('/generate/payment-wo/{id}', [WorkOrderController::class, 'generatePaymentWO']);
         Route::get('/show/billing/{id}', [WorkOrderController::class, 'showBilling']);
 
