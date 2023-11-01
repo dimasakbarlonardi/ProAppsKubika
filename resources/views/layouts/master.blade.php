b429d644f473d4fb6fd5b0d74fe22f58e24fcb66
akmal <satoefrekuensi@gmail.com>
2023-11-01 15:54:32 +0700

    WEB | Revamp reservation

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -419,6 +419,7 @@
         }
 
         function getNewNotifications(user_id, receiver, division_receiver, notif_id) {
+            console.log(user_id, receiver, division_receiver, notif_id);
             $.ajax({
                 url: `/admin/get-new-notifications/${notif_id}`,
                 type: 'GET',
