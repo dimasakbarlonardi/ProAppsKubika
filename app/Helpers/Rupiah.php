3c01467a5d36b19e8d1756ab3f30d071b43fdcf0
akmal <satoefrekuensi@gmail.com>
2023-11-03 14:13:44 +0700

    WEB | Fix fin Utilities

--- app/Helpers/Rupiah.php
+++ app/Helpers/Rupiah.php
@@ -1,8 +1,21 @@
-<?php 
+<?php
 
 function rupiah($angka){
-	
 	$hasil_rupiah = "Rp " . number_format($angka,0,',','.');
 	echo $hasil_rupiah;
- 
-}	
+}
+
+function RupiahNumber($angka){
+	$hasil_rupiah = number_format($angka,0,',','.');
+	echo $hasil_rupiah;
+}
+
+function DecimalRupiah($angka) {
+    $hasil_rupiah = number_format($angka,2,',','.');
+	echo $hasil_rupiah;
+}
+
+function DecimalRupiahRP($angka) {
+    $hasil_rupiah = "Rp " . number_format($angka,2,',','.');
+	echo $hasil_rupiah;
+}
--- resources/views/AdminSite/IPLType/index.blade.php
+++ resources/views/AdminSite/IPLType/index.blade.php
@@ -7,9 +7,9 @@
             <div class="my-3 col-auto">
                 <h6 class="mb-0">List IPL Type</h6>
             </div>
-            <div class="col-auto d-flex">
+            {{-- <div class="col-auto d-flex">
                 <a class="btn btn-falcon-default btn-sm text-600" href="{{ route('ipltypes.create') }}"><span class="fas fa-plus fs--2 me-1"></span>Create IPL Type</a>
-            </div>
+            </div> --}}
         </div>
     </div>
     <div class="p-5">
@@ -26,18 +26,18 @@
             <tbody>
                 @foreach ($ipltypes as $key => $ipltype)
                     <tr>
-                        <th scope="row">{{ $key + 1 }}</th> 
+                        <th scope="row">{{ $key + 1 }}</th>
                         <td>{{ $ipltype->nama_ipl_type }}</td>
                         <td>{{ $ipltype->biaya_permeter ? rupiah($ipltype->biaya_permeter) : '-' }}</td>
                         <td>{{ $ipltype->biaya_procentage ? persen($ipltype->biaya_procentage) : '-'  }} </td>
                         <td>
                             <a href="{{ route('ipltypes.edit', $ipltype->id_ipl_type) }}" class="btn btn-sm btn-warning"><span class="fas fa-pencil-alt fs--2 me-1"></span>Edit</a>
-                            <form class="d-inline" action="{{ route('ipltypes.destroy', $ipltype->id_ipl_type) }}" method="post">
+                            {{-- <form class="d-inline" action="{{ route('ipltypes.destroy', $ipltype->id_ipl_type) }}" method="post">
                                 @method('DELETE')
                                 @csrf
                                 <button type="submit" class="btn btn-danger btn-sm"
                                     onclick="return confirm('are you sure?')"><span class="fas fa-trash-alt fs--2 me-1"></span>Hapus</button>
-                            </form>
+                            </form> --}}
                         </td>
                     </tr>
                 @endforeach
