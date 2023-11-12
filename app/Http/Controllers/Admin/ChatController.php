4a5ba6a350d7878227b2ca79e7bd9dd85b00e132
akmal <satoefrekuensi@gmail.com>
2023-11-12 23:46:44 +0700

    WEB | Chat tenant and TR

---
+++ app/Http/Controllers/Admin/ChatController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Events\ChatEvent;
+use App\Helpers\ConnectionDB;
+use App\Http\Controllers\Controller;
+use App\Models\Chat;
+use App\Models\RoomChat;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+
+class ChatController extends Controller
+{
+    public function index(Request $request)
+    {
+        $connUser = ConnectionDB::setConnection(new User());
+        $connRoomChat = ConnectionDB::setConnection(new RoomChat());
+
+        $user = $connUser->where('login_user', $request->user()->email)->first();
+        $role = $user->RoleH->work_relation_id;
+
+        if ($role == 1) {
+            $data['rooms'] = $connRoomChat->orderBy('updated_at', 'DESC')->get();
+        } else {
+            $data['room'] = $connRoomChat->where('sender_id', $request->session()->get('user_id'))->first();
+        }
+
+        return view('AdminSite.Chat.index', $data);
+    }
+
+    public function rooms()
+    {
+        $connRoomChat = ConnectionDB::setConnection(new RoomChat());
+
+        $data['rooms'] = $connRoomChat->orderBy('updated_at', 'DESC')->get();
+
+        return response()->json([
+            'html' => view('AdminSite.Chat.rooms', $data)->render()
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $connChat = ConnectionDB::setConnection(new Chat());
+        $connUser = ConnectionDB::setConnection(new User());
+        $connRoomChat = ConnectionDB::setConnection(new RoomChat());
+
+        $sender = $connUser->where('login_user', $request->user()->email)->first();
+
+        $isExist = $connRoomChat->find($request->room_id);
+
+        if (!$isExist) {
+            $connRoomChat->id = $request->room_id;
+            $connRoomChat->receiver_id = $request->receiver_id_value;
+            $connRoomChat->sender_id = $request->sender_id_value;
+            $connRoomChat->save();
+        }
+
+        $connChat->room_id = $isExist ? $request->room_id : $connRoomChat->id;
+        $connChat->message = $request->value;
+        $connChat->receiver_id = $request->receiver_id_value;
+        $connChat->sender_id = $sender->id_user;
+        $connChat->is_read = false;
+        $connChat->save();
+
+        if ($isExist) {
+            $isExist->updated_at = Carbon::now();
+            $isExist->save();
+        }
+
+        $this->sendNotif($connChat, true);
+    }
+
+    public function getChats(Request $request)
+    {
+        $connRoom = ConnectionDB::setConnection(new RoomChat());
+
+        $data['room'] = $connRoom->find($request->room_id);
+
+        return response()->json([
+            'html' => view('AdminSite.Chat.chat-room', $data)->render()
+        ]);
+    }
+
+    public function roomSlave(Request $request)
+    {
+        $connRoom = ConnectionDB::setConnection(new RoomChat());
+        $connUser = ConnectionDB::setConnection(new User());
+
+        $user = $connUser->where('login_user', $request->user()->email)->first();
+
+        $data['room'] = $connRoom->where('sender_id', $user->id_user)->first();
+
+        return response()->json([
+            'html' => view('AdminSite.Chat.chat-room-slave', $data)->render()
+        ]);
+    }
+
+    public function roomMaster(Request $request)
+    {
+        $connRoom = ConnectionDB::setConnection(new RoomChat());
+
+        $data['room'] = $connRoom->find($request->room_id);
+
+        return response()->json([
+            'html' => view('AdminSite.Chat.chat-room-master', $data)->render(),
+            'chat' => $data['room']
+        ]);
+    }
+
+    public function readChats(Request $request)
+    {
+        $connRoom = ConnectionDB::setConnection(new RoomChat());
+
+        $room = $connRoom->find($request->room_id);
+
+        foreach ($room->Chats as $chat) {
+            $chat->is_read = true;
+            $chat->save();
+        }
+
+        $this->sendNotif($chat, false);
+    }
+
+    public function sendNotif($chat, $sound)
+    {
+        $dataNotif = [
+            'room' => $chat->room_id,
+            'receiver' => $chat->receiver_id,
+            'sender' => $chat->sender_id,
+            'sound' => $sound,
+        ];
+        broadcast(new ChatEvent($dataNotif));
+    }
+}
---
+++ resources/views/AdminSite/Chat/chat-room-slave.blade.php
@@ -0,0 +1,44 @@
+@if ($room)
+    <div class="chat-content-scroll-area scrollbar" id="content-message-body">
+        @foreach ($room->Chats as $chat)
+            @if (Request::session()->get('user_id') == $chat->sender_id)
+                <div class="d-flex p-3">
+                    <div class="flex-1 d-flex justify-content-end">
+                        <div class="w-100 w-xxl-75">
+                            <div class="hover-actions-trigger d-flex flex-end-center">
+                                <div class="bg-info text-white p-2 rounded-2 chat-message" data-bs-theme="light">
+                                    {{ $chat->message }}
+                                </div>
+                            </div>
+                            <div class="text-400 fs--2 text-end">
+                                {{ HumanDateTime($chat->created_at) }}<span
+                                    class="fas fa-check ms-2 text-success"></span></div>
+                        </div>
+                    </div>
+                </div>
+            @else
+                <div class="d-flex p-3">
+                    <div class="avatar avatar-l me-2">
+                        <img class="rounded-circle"
+                            src="{{ $chat->Sender->profile_picture ? url($chat->Sender->profile_picture) : url('/storage/img/proapps.png') }}"
+                            alt="" />
+                    </div>
+                    <div class="flex-1">
+                        <div class="w-xxl-75">
+                            <div class="hover-actions-trigger d-flex align-items-center">
+                                <div class="chat-message bg-200 p-2 rounded-2">
+                                    {{ $chat->message }}
+                                </div>
+                            </div>
+                            <div class="text-400 fs--2">
+                                <span>{{ HumanDateTime($chat->created_at) }}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endif
+        @endforeach
+    </div>
+    <input type="hidden" id="room_id_value" value="{{ $room->id }}">
+    <input type="hidden" id="receiver_id_value" value="{{ $room->Receiver->id_user }}">
+@endif
