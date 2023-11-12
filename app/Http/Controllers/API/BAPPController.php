34f9605da44882ffa5bd1a343edc9b7502b52ced
akmal <satoefrekuensi@gmail.com>
2023-11-13 00:05:27 +0700

    API | BAPP Show

--- app/Http/Controllers/API/BAPPController.php
+++ app/Http/Controllers/API/BAPPController.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Helpers\ConnectionDB;
+use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use App\Models\BAPP;
 use Illuminate\Http\Request;
 
 class BAPPController extends Controller
 {
-    //
+    public function show($id)
+    {
+        $connBAPP = ConnectionDB::setConnection(new BAPP());
+
+        $bapp = $connBAPP->find($id);
+
+        return ResponseFormatter::success(
+            $bapp,
+            'Success get bapp'
+        );
+    }
 }
