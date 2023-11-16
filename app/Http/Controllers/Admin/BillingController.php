f5963233faff9c0d7eee777b1c8a20d436db9dd4
akmal <satoefrekuensi@gmail.com>
2023-11-16 11:44:10 +0700

    API | Tracking ticket complaint

--- app/Http/Controllers/Admin/BillingController.php
+++ app/Http/Controllers/Admin/BillingController.php
@@ -539,9 +539,10 @@ class BillingController extends Controller
                         "unit" => "day"
                     ]
                 ]);
+
                 $response = json_decode($response->getBody());
 
-                if ($response->status_code != 406) {
+                if ($response->status_code == 201) {
                     $transaction->va_number = $response->va_numbers[0]->va_number;
                     $transaction->expiry_time = $response->expiry_time;
                     $transaction->transaction_id = $response->transaction_id;
