7410a84c70af754fb5a7cf3de3f2229ee81b5e0f
akmal <satoefrekuensi@gmail.com>
2024-01-17 22:29:42 +0700

    WEB | Fix upload utility image

--- app/Helpers/SaveFile.php
+++ app/Helpers/SaveFile.php
@@ -28,10 +28,7 @@ class SaveFile
         $fileName = $type . '-' . Carbon::now()->format('Y-m-d') . '-' .   $file->getClientOriginalName();
         $path = '/public/' . $idSite . '/img/' . $type . '/' . $fileName;
         $storagePath = '/storage/' . $idSite . '/img/' . $type .  '/' . $fileName;
-        $img = Image::make($file->getRealPath());
-        $img->resize(100, 100, function ($constraint) {
-            $constraint->aspectRatio();
-        })->encode('jpg', 80);
+        $img = Image::make($file->getRealPath())->encode('jpg', 80);
 
         Storage::disk('local')->put($path, $img);
 
